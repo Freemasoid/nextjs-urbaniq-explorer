c9163bb41e84ffb615a508ed69a93a0904e4ebae
rm-reins <rm_reins@icloud.com>
2025-05-05 16:18:24 +0200

    feat: new tour page base setup

--- src/i18n/languages/enUS.ts
+++ src/i18n/languages/enUS.ts
@@ -1,1 +1,26 @@
-export const enUS = {};
+export const enUS = {
+  chat: {
+    askGuide: "Ask your guide ...",
+    assist: "Hello! How can I assist you today?",
+  },
+  newTour: {
+    generateTour: "Generate Tour",
+    generating: "Generating ...",
+    success: "Tour generated successfully",
+    fail: "There was an error generating a tour",
+    create: "Create new tour",
+    whereTo: "Where do you want to go?",
+    whereToDescription:
+      "Enter a city and country to generate a personalized tour itinerary.",
+    city: "City",
+    cityPlaceholder: "e.g. Berlin",
+    country: "Country",
+    countryPlaceholder: "e.g. Germany",
+    travelSuggestions: "Travel Suggestions",
+  },
+  tours: {},
+  profile: {},
+  errors: {
+    cityCountryEmpty: "Please enter both city and country",
+  },
+};
