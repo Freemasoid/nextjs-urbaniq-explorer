89cd4c955b8c4af973eca704016456fd62ff3606
rm-reins <rm_reins@icloud.com>
2025-05-22 16:25:30 +0200

    feat: add Card, Input, Label, and Separator components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,40 @@
 import Header from "./Header";
 import { Button } from "./Button";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./Card";
+import { Input } from "./Input";
+import { Label } from "./Label";
+import { Separator } from "./Separator";
+import { Toaster, toast } from "./Sonner";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./Tooltip";
 
-export { Header, Button };
+export {
+  Header,
+  Button,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  Input,
+  Label,
+  Separator,
+  Toaster,
+  toast,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+};
